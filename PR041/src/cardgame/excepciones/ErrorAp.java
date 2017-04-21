d2a691dff56a57ba9433d88063716d989bbbfae9
Jesus <Jesus@192.168.1.131>
2017-04-21 10:15:34 +0200

    Creada la clase ErrorAp que se encargará de gestionar las excepciones

---
+++ PR041/src/cardgame/excepciones/ErrorAp.java
@@ -0,0 +1,74 @@
+package cardgame.excepciones;
+
+/**
+ * Clase encargada de la gestión de excepciones de la aplicación.
+ * @author Jesús Rivero Muñiz
+ */
+@SuppressWarnings("oracle.jdeveloper.java.serialversionuid-field-missing")
+public class ErrorAp extends Exception{
+    
+    /**
+     * Código del error.
+     */
+    private int codError;
+    
+    /**
+     * Mensaje del error.
+     */
+    private String msgError;
+    
+    /**
+     * Tipo de excepción del error.
+     */
+    private Exception excepcion;
+    
+    /**
+     * Constructor que crea un objeto ErrorAp a través de un código
+     * y un mensaje de error.
+     * @param codError Código del error.
+     * @param msgError Mensaje del error.
+     */
+    public ErrorAp(int codError, String msgError) {
+        super(msgError);
+        this.codError = codError;
+        this.msgError = msgError;
+    }
+    
+    /**
+     * Constructor que crea un objeto ErrorAp a través de un código,
+     * un mensaje y a su vez encapsula una excepción.
+     * @param codError Código del error.
+     * @param msgError Mensaje del error.
+     * @param excepcion Excepción obtenida.
+     */
+    public ErrorAp(int codError, String msgError, Exception excepcion) {
+        super(msgError, excepcion);
+        this.codError = codError;
+        this.msgError = msgError;
+        this.excepcion = excepcion;
+    }
+    
+    /**
+     * Método getter de la propiedad codError.
+     * @return Valor de la propiedad codError.
+     */
+    public int getCodError() {
+        return codError;
+    }
+    
+    /**
+     * Método getter de la propiedad msgError.
+     * @return Valor de la propiedad msgError.
+     */
+    public String getMsgError() {
+        return msgError;
+    }
+    
+    /**
+     * Método getter de la propiedad excepcion.
+     * @return Valor de la propiedad excepcion.
+     */
+    public Exception getExcepcion() {
+        return excepcion;
+    }
+}

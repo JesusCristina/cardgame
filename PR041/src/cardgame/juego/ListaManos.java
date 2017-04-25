20e411c3583f2198b41bf6011d0a37a6079ba3c7
Tinita <Tinita@Tina>
2017-04-25 21:17:33 +0200

    ListaMaos - añadida documentación Javadoc

--- PR041/src/cardgame/juego/ListaManos.java
+++ PR041/src/cardgame/juego/ListaManos.java
@@ -11,21 +11,35 @@ import java.util.ArrayList;
  * Esta clase tendrÃ¡ un contenedor de manos. TendrÃ¡n que desarrollarse
  * los mÃ©todos bÃ¡sicos para trabajar con el contenedor.
  */
+
+/**
+ * Clase contenedora que guarda una lista de manos.
+ * @author Cristina Polo Sánchez.
+ */
 public class ListaManos {
     private ArrayList<Mano> listaManos;
     
-    /*Contenedor de lista mano*/
+    
+    /**
+     * Contenedor interno de esta clase en el que se almacena una
+     * lista de manos. El resto de clases trabajarán con "mano", no 
+     * directamente con este contenedor.
+     */
     public ListaManos() {
         listaManos = new ArrayList<Mano>();  
     }       
     
+    /**
+     * Método que sirve para agregar una nueva mano a la lista.
+     * @param mano Añade una mano.
+     */
     public void agregarMano (Mano mano){
         listaManos.add(mano);
     }
     
-    /*esto es un contenedor de manos*/
     /**
-     * 
+     * Contenedor de manos, que es con la que trabajarían el resto de 
+     * clases. 
      * @param posicion
      * @return
      */
@@ -34,10 +48,21 @@ public class ListaManos {
         return mano;
     }
     
+    /**
+     * Se elimina una mano de la lista. Cada mano se diferencia de las 
+     * demás por la posición que ocupa en la lista.
+     * @param posicion Posición (expresada en un número entero) de una
+     * mano en concreto en la lista de manos.
+     */
     public void eilminaMano(int posicion) {
         listaManos.remove(posicion);        
     }
     
+    /**
+     * Proporciona información sobre el tamaño de la lista de manos.
+     * @return Devuelve, en números enteros, el tamaño de la lista de 
+     * manos.
+     */
     public int size() {
         return listaManos.size();
     }    

4df67c0df1f6b0cdcc0146623cc2328165fc9574
Tinita <Tinita@Tina>
2017-04-25 22:01:43 +0200

    Implementación y documentación de la clase Mano

--- PR041/src/cardgame/juego/Mano.java
+++ PR041/src/cardgame/juego/Mano.java
@@ -1,8 +1,22 @@
 package cardgame.juego;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
-//TODO Documentación e implementación
 public class Mano {
+    
+    /**
+     * Encapsula las manos de cartas del juego.
+     * Una mano es un conjunto de 5 cartas.
+     * @author Cristina Polo S�nchez.
+     */
     private LinkedList<Carta> cartas;
+
+    /**
+     * Devuelve las cartas de la mano.
+     * @return Las cartas de la mano.
+     */
+    private Iterator<Carta> getCartas() {
+        return cartas.iterator();
+    }
 }

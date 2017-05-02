0a818c18a364094a7235a9f00802cdad2a5f4d2b
Cristina Polo <roconuno@gmail.com>
2017-05-02 18:56:19 +0200

    Implementación y documentación de las propiedades de la clase Juego. Documentación de la clase Partida

--- PR041/src/cardgame/juego/Juego.java
+++ PR041/src/cardgame/juego/Juego.java
@@ -1,6 +1,54 @@
 package cardgame.juego;
 
+import cardgame.bd.DAOImpl;
+import cardgame.util.Menu;
+import cardgame.util.UtilidadesES;
+
+
+/**
+ * Clase contenedora de la mecánica del juego.
+ */
 public class Juego {
+    
+    /**
+     * Contenedor del menú del juego.
+     */
+    private Menu menu;
+    
+    /**
+     * Contenedor de las utilidades del juego.
+     */
+    private UtilidadesES utilidadesES;
+    
+    /**
+     * Contenedor del mazo para jugar.
+     */
+    private Mazo mazo;
+    
+    /**
+     * Contenedor DAO.
+     */
+    private DAOImpl daoImpl;
+    
+    /**
+     * Contenedor con la lista de jugadores.
+     */
+    private ListaJugadores listaJugadores;
+    
+    /**
+     * Contenedor de la lista de partidas.
+     */
+    private ListaPartidas listaPartidas;
+    
+    /**
+     * Contenedor de la lista de manos.
+     */
+    private ListaManos listaManos;
+    
+    /**
+     * Método que inicializa el juego.
+     */
     public void ejecutar() {
     }
 }
+
--- PR041/src/cardgame/juego/Partida.java
+++ PR041/src/cardgame/juego/Partida.java
@@ -2,12 +2,34 @@ package cardgame.juego;
 
 import java.util.LinkedList;
 
+/**
+ * Clase que encapsula el concepto de partida en el juego.
+ * @author Cristina Polo Sánchez.
+ */
 public class Partida {
+    
+    /**
+     * Número de la partida.
+     */
     private int numPartida;
+    
+    /**
+     * Lista de jugadores.
+     */
     private LinkedList<Jugador> jugadores;
+    
+    /**
+     * El contenedor de manos.
+     */
     private LinkedList<Mano> resultado;
     
     
+    /**
+     * Método por el que se almacena cada uno de los datos pertinentes para cada partida.
+     * @param numPartida El número de partidas.
+     * @param jugadores La lista de jugadores.
+     * @param resultado El contenedor de manos
+     */
     public Partida (int numPartida, LinkedList<Jugador> jugadores, LinkedList<Mano> resultado) {
     this.numPartida = numPartida;
     this.jugadores = jugadores;

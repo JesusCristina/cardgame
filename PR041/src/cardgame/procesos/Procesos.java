543f6f5bcf43b56662ef5a105f3b28bb5bd6446b
Jesús Rivero Muñiz <jesusriverociclo@gmail.com>
2017-05-05 19:20:17 +0200

    Comentada la interfaz Procesos

--- PR041/src/cardgame/procesos/Procesos.java
+++ PR041/src/cardgame/procesos/Procesos.java
@@ -1,5 +1,17 @@
 package cardgame.procesos;
 
+import java.io.IOException;
+
+/**
+ * Clase orientada al procesamiento de las diferentes
+ * opciones del juego.
+ * @author Jesús Rivero Muñiz
+ */
 public interface Procesos {
-    public void procesar();
+    
+    /**
+     * Método que procesa una opción determinada.
+     * @param opcion Opción a procesar
+     */
+    public void procesar(int opcion) throws IOException;
 }
